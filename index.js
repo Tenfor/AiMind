2d3f8245add5d1349881965fd6af04c9d363b4b9
Tenfor <f.zsolt95@gmail.com>
2023-02-17 15:11:50 +0100

    tewaked message endpoint + new model

--- index.js
+++ index.js
@@ -12,21 +12,41 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/message',(req,res)=>{
-    console.log("message arrived: "+req.body.prompt);
-    const response = openai.createCompletion({
-        model: 'text-curie-001',
-        prompt: req.body.prompt,
-        temperature: 0,
-        top_p: 1,
-        frequency_penalty:0, 
-        presence_penalty: 0,
-        max_tokens: 100
-    });
+// async function chatWithBot(message) {
+//     const prompt = `Beszélgetés a bot-tal: "${message}".\nBot válasza:`;
+  
+//     const completions = await openai.completions.create({
+//       engine: MODEL_ENGINE,
+//       prompt,
+//       maxTokens: 100,
+//       n: 1,
+//       stop: '\n',
+//     });
+  
+//     const answer = completions.choices[0].text.trim();
+//     return answer;
+//   }
 
-    response.then((data)=>{
-        res.send({message:data.data.choices[0].text});
-    });
+app.post('/message',(req,res)=>{
+    try {
+        const msg = req.body.msg;
+        const prompt = `Beszélgetés a bot-tal: "${msg}".\nBot válasza:`;
+        console.log("message arrived: "+msg);
+        const response = openai.createCompletion({
+            model: 'text-davinci-003',
+            prompt: prompt,
+            n: 1,
+            stop:'\n', 
+            max_tokens: 100
+        });
+    
+        response.then((data)=>{
+            res.send({message:data.data.choices[0].text.trim()});
+        });
+    } catch (error) {
+        console.error(error);        
+    }
+
 });
 
 app.listen(3000, ()=>{
