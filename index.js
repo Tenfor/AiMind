30a4b57570dfe5f8e4ff0a87b4fad2966bff19dd
Tenfor <f.zsolt95@gmail.com>
2023-02-18 22:13:23 +0100

    grammar fix tinker

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ async function fixMessage(msg){
         prompt: prompt,
         temperature: 0,
         max_tokens: 60,
-        top_p: 1.0,
         frequency_penalty: 0.0,
         presence_penalty: 1.0,
     };
